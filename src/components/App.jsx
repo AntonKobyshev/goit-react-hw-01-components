74142e518287ab29197626c27f2f0cc7e41e2df3
AntonKobyshev <antonkobyshev@gmail.com>
2023-04-25 19:23:00 +0300

    updt

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import user from './Data/user';
+import user from './Profile/user.json';
 import { Profile } from './Profile/Profile';
 import data from './Data/data.json';
 import { Statistics } from './Statistics/Statistics';
