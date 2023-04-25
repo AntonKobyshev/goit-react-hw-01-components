ebac588b8a74592396b991a2a897821b6127f991
AntonKobyshev <antonkobyshev@gmail.com>
2023-04-25 19:53:26 +0300

    updt

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
-import user from './ata/user.json';
+import user from './data/user.json';
 import { Profile } from './Profile/Profile';
-import data from './ata/data.json';
+import data from './data/data.json';
 import { Statistics } from './Statistics/Statistics';
-import friends from './ata/friends.json';
+import friends from './data/friends.json';
 import { FriendList } from './Friends/FriendList/FriendList';
-import transactions from './ata/transactions.json';
+import transactions from './data/transactions.json';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import { Section } from './Section';
 
